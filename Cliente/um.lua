74f918db590dc4e11f8a6433902c5693238f7f8e
walissonsilva <walissonsilva10@gmail.com>
2016-03-22 21:04:47 -0300

    Versão 16.3.2

---
+++ Cliente/um.lua
@@ -0,0 +1,7 @@
+-- Escrever numero 1
+  disp:drawVLine(75, 25, 32);
+  disp:drawVLine(76, 25, 32);
+  disp:drawVLine(77, 25, 32);
+  disp:drawVLine(78, 25, 32);
+  disp:drawVLine(79, 25, 32);
+  disp:drawBox(72, 25, 3, 5);
--- Server/server.lua
+++ Server/server.lua
@@ -1,7 +1,8 @@
+-- ESP01
 -- Server
 
 -- LEDs
-DHT11 = 3; -- GPIO0
+DHT11 = 1; -- GPIO0
 temp_anterior = 0;
 LEDR = 4; -- GPIO2
 
@@ -9,9 +10,9 @@ gpio.mode(LEDR, gpio.OUTPUT);
 --
 
 -- Configurando o modo de operacao do wifi
-wifi.setmode(wifi.STATIONAP);
+wifi.setmode(wifi.SOFTAP);
 cfg={}
-cfg.ssid = "ESP01"
+cfg.ssid = "ESP"
 cfg.pwd = "eletromagnetismo"
 wifi.ap.config(cfg)
 --
@@ -19,25 +20,27 @@ wifi.ap.config(cfg)
 -- Imprimir o endereco de IP do servidor
 print("Server IP Address: ", wifi.ap.getip());
 
-sv = net.createServer(net.TCP) 
+sv = net.createServer(net.TCP, 28800) 
 sv:listen(80, function(conn)
-    conn:on("receive", function(conn, dadoRecebido) 
-        if (dadoRecebido == "LEDG") then
-            gpio.write(LEDR, gpio.LOW);
-        elseif (dadoRecebido == "LEDR") then
-            gpio.write(LEDR, gpio.HIGH);
-        end
-    end)
-    
-    conn:on("sent", function(conn) 
+    --conn:on("receive", function(conn, dadoRecebido) 
+    --    if (dadoRecebido == "LEDG") then
+    --        gpio.write(LEDR, gpio.LOW);
+    --    elseif (dadoRecebido == "LEDR") then
+    --        gpio.write(LEDR, gpio.HIGH);
+    ---    end
+    --end)
+
+    conn:on("sent", function(con) 
+      --conn:close()
+      --tmr.stop(0)
       collectgarbage()
     end)
-end)
 
-tmr.alarm(0, 10000, 1, function() 
-    getTemp()
-    print(temp)
-    sv:send(temp);
+    tmr.alarm(0, 10000, 1, function() 
+      getTemp()
+      print(temp_anterior)
+      conn:send(temp_anterior);
+    end)
 end)
 
 function getTemp()
